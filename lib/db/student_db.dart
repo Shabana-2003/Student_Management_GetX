b29110ccba2d17232088b1ff3d50086ad82e6f6a
Shabana MA <mashabana751@gmail.com>
2024-03-03 13:52:18 +0530

    getx student app completed

--- lib/db/student_db.dart
+++ lib/db/student_db.dart
@@ -11,23 +11,21 @@ class Student extends HiveObject {
       required this.domain,
       required this.number});
 
-      @HiveField(0)
-      int? id;
+  @HiveField(0)
+  int? id;
 
-      @HiveField(1)
-      final String image;
+  @HiveField(1)
+  final String image;
 
-      @HiveField(2)
-      final String name;
+  @HiveField(2)
+  final String name;
 
-      @HiveField(3)
-      final String age;
+  @HiveField(3)
+  final String age;
 
-      @HiveField(4)
-      final String domain;
+  @HiveField(4)
+  final String domain;
 
-      @HiveField(5)
-      final String number;
-
-
+  @HiveField(5)
+  final String number;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,18 @@ import 'package:student_management/controller/student_controller.dart';
 import 'package:student_management/db/student_db.dart';
 import 'package:student_management/pages/bottom_nav.dart';
 
-late Box<Student> studentDb;
-final StudentController studentController = Get.put(StudentController());
+late Box<Student> studentDb;  //late : the variable will be initialized later before its first use.
+// Box : This box is going to hold instances of the Student class.
+final StudentController studentController = Get.put(StudentController());// studentController: variable // Get.put(StudentController() : This part initializes an instance of the StudentController class.
+//Get.put(): This is a method provided by the Get package. It's like a magic wand that creates an instance of the specified class. By passing StudentController() inside Get.put(), we're telling Get 
+//to create an instance of StudentController and put it into a special storage space that Get manages.
+
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();  // Ensure that Flutter widgets are initialized
   await Hive.initFlutter();
-  Hive.registerAdapter(StudentAdapter());
-  studentDb = await Hive.openBox<Student>('student');
+  Hive.registerAdapter(StudentAdapter());  // Register the adapter for the Student class
+  studentDb = await Hive.openBox<Student>('student');  // Open a box named 'student' to store Student objects
   runApp(const MyApp());
 }
 
